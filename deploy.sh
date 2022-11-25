fff5a8d07520e88522007fa28aa777073628b076
hoollyzhang <hoollyzhang@tencent.com>
2022-11-25 22:56:29 +0800

    feat: 做点修改

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:bravekingzhang/web3.git master:gh-pages
+git push -f git@github.com:bravekingzhang/w3bprivacy.git master:gh-pages
 
 cd -
