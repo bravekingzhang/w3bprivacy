dea90ac51c006e7fa2437e18cf2c9c24fecf6686
hoollyzhang <hoollyzhang@tencent.com>
2022-11-25 22:42:46 +0800

    feat:test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,7 @@
 module.exports = {
   title: "w3bprivacy",
   description: "Just doing something about web3.0,sample and safe access",
-  base: "/w3bprivacy/",
-  build: {
-    outDir: "build",
-  },
+  base: "/web3/",
   markdown: {
     // options for markdown-it-anchor
     // anchor: { permalink: false },
